97936c45d798b990c736463df9fd526e292e7554
Vitaliy Vorobyov <vorobyov@ucu.edu.ua>
2018-02-23 14:42:27 +0200

    Update map_create.py

--- map_create.py
+++ map_create.py
@@ -1,10 +1,12 @@
 import folium
 from geopy import ArcGIS
 from random import randint
+from folium.plugins import MarkerCluster
 
 
 def main(user_lst, friends_lst, num):
     map_html = folium.Map()
+    marker_cluster = MarkerCluster().add_to(map_html)
     geolocator = ArcGIS(timeout=10)
     lst = ["Name: ", "Location: ", "Description: ", "url: ",
            "Followers: ", "Friends: ", "Listed: ", "Created: ",
@@ -17,7 +19,7 @@ def main(user_lst, friends_lst, num):
         popup_message = '<img src="{}"/><br>'.format(user_lst[-1])
         for i in range(len(user_lst) - 1):
             popup_message += "{}{}<br>".format(lst[i], user_lst[i])
-        map_html.add_child(
+        marker_cluster.add_child(
             folium.Marker(location=[randint(-90, 90), randint(-180, 180)],
                           popup=popup_message,
                           icon=folium.Icon(color='green')))
@@ -25,7 +27,7 @@ def main(user_lst, friends_lst, num):
         popup_message = '<img src="{}"/><br>'.format(user_lst[-1])
         for i in range(len(user_lst) - 1):
             popup_message += "{}{}<br>".format(lst[i], user_lst[i])
-        map_html.add_child(folium.Marker(location=[place.latitude, place.longitude],
+        marker_cluster.add_child(folium.Marker(location=[place.latitude, place.longitude],
                                          popup=popup_message,
                                          icon=folium.Icon(color='green')))
 
@@ -37,7 +39,7 @@ def main(user_lst, friends_lst, num):
             popup_message = '<img src="{}"/><br>'.format(friends_lst[i][-1])
             for j in range(len(friends_lst[i]) - 1):
                 popup_message += "{}{}<br>".format(lst[j], friends_lst[i][j])
-            map_html.add_child(
+            marker_cluster.add_child(
                 folium.Marker(location=[randint(-90, 90), randint(-180, 180)],
                               popup=popup_message,
                               icon=folium.Icon(color='red')))
@@ -45,7 +47,7 @@ def main(user_lst, friends_lst, num):
             popup_message = '<img src="{}"/><br>'.format(friends_lst[i][-1])
             for j in range(len(friends_lst[i]) - 1):
                 popup_message += "{}{}<br>".format(lst[j], friends_lst[i][j])
-            map_html.add_child(
+            marker_cluster.add_child(
                 folium.Marker(location=[place.latitude, place.longitude],
                               popup=popup_message,
                               icon=folium.Icon(color='red')))
