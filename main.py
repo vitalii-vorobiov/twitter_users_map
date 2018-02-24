d018ce7afe13947371debd8d546c6a4b162e35cf
Vitaliy Vorobyov <vorobyov@ucu.edu.ua>
2018-02-24 17:45:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,13 +14,16 @@ def main():
 
 @app.route('/',methods=["POST"])
 def input_page():
-    user_id = request.form['user_id']
-    num = request.form['friends']
-    user_lst = user.main(user_id)
-    friends_lst = twitter2.main(user_id, num)
-    map_create.main(user_lst, friends_lst, num)
-    app.jinja_env.auto_reload = True
-    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    try:
+        user_id = request.form['user_id']
+        num = request.form['friends']
+        user_lst = user.main(user_id)
+        friends_lst = twitter2.main(user_id, num)
+        map_create.main(user_lst, friends_lst, num)
+        app.jinja_env.auto_reload = True
+        app.config['TEMPLATES_AUTO_RELOAD'] = True
+    except:
+        return render_template('user_not_found.html')
     return render_template('map_html.html')
 
 
